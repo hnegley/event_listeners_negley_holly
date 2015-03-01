74e91202b1337c2947545c2a5e84a9ac6e805cb0
Holly Negley <holly.negley@icloud.com>
2015-02-28 20:53:12 -0500

    added click me

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var toClick = document.getElementsByTagName("h1"); 
+
+toClick[0].addEventListener( "click", function() {
+    var newPara = document.createElement( "p" ); 
+    newPara.innerHTML = "This is click number x";
+    
+    var theContainer = document.getElementsByTagName( "container" );
+    theContainer.appendChild( newPara );
+    
+}
