9c839176b7664ed0f125fd55a7ec3a18e79ae309
Richard Kalehoff <richardkalehoff@gmail.com>
2015-03-05 00:13:23 -0500

    make event listener keep track of number of clicks

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 var toClick = document.getElementsByTagName("h1");
 
+var clickCount = 1;
+
 toClick[0].addEventListener( "click", function() {
     var newPara = document.createElement( "p" );
-    newPara.innerHTML = "This is click number x";
+    newPara.innerHTML = "This is click number " + clickCount;
+
+    clickCount = clickCount + 1;
 
     var theContainer = document.body;
     theContainer.appendChild( newPara );
