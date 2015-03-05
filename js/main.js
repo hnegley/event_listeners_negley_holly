06e7d1546365af6524b1c004fce7000bdb366722
Richard Kalehoff <richardkalehoff@gmail.com>
2015-03-05 00:11:28 -0500

    perform an initial save which might mess with the code

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-var toClick = document.getElementsByTagName("h1"); 
+var toClick = document.getElementsByTagName("h1");
 
 toClick[0].addEventListener( "click", function() {
-    var newPara = document.createElement( "p" ); 
+    var newPara = document.createElement( "p" );
     newPara.innerHTML = "This is click number x";
-    
+
     var theContainer = document.getElementsByTagName( "container" );
     theContainer.appendChild( newPara );
     }
