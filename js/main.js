38f53145ec5812cddc801602a2c8a844b83af934
Richard Kalehoff <richardkalehoff@gmail.com>
2015-03-05 00:14:03 -0500

    perform save which might mess with formatting

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-var fillOut = document.getElementsByTagName("submit"); 
+var fillOut = document.getElementsByTagName("submit");
 
 fillOut[0].addEventListener( "click", function() {
-    var newPara = document.createElement( "p" ); 
+    var newPara = document.createElement( "p" );
     newPara.innerHTML = "What a beautiful site!";
-    
+
     var theContainer = document.getElementsByTagName( "container" );
     theContainer.appendChild( newPara );
     }
